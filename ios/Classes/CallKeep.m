288e2a070a423b8608684bd1135df38efaf218e7
BradenBagby <bradenjbagby@gmail.com>
2023-09-28 12:36:12 -0600

    Revert "removes logs"
    This reverts commit 23b0d26e6b8958d7a4fd7829f386809aa1dcf627.

--- ios/Classes/CallKeep.m
+++ ios/Classes/CallKeep.m
@@ -228,6 +228,7 @@ static CXProvider* sharedProvider;
 - (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(nonnull void (^)(void))completion {
     // Process the received push
     NSLog(@"didReceiveIncomingPushWithPayload payload = %@", payload.type);
+    [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"Received payload"]];
     
     [RTCAudioSession sharedInstance].useManualAudio = true;
     [RTCAudioSession sharedInstance].isAudioEnabled = false;
@@ -237,21 +238,25 @@ static CXProvider* sharedProvider;
         NSLog(@"configuring audio session..");
         [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
         if (err) {
+            [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"failed to set audio category %@", err]];
             NSLog(@"error setting audio category %@",err);
         }
         [audioSession setMode:AVAudioSessionModeVoiceChat error:&err];
         if (err) {
+            [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"failed to set audio mode %@", err]];
             NSLog(@"error setting audio Mode %@",err);
         }
         double sampleRate = 44100.0;
         [audioSession setPreferredSampleRate:sampleRate error:&err];
         if (err) {
+            [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"failed to set ample rate %@", err]];
             NSLog(@"Error %ld, %@",(long)err.code, err.localizedDescription);
         }
         
         NSTimeInterval bufferDuration = .005;
         [audioSession setPreferredIOBufferDuration:bufferDuration error:&err];
         if (err) {
+            [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"failed to set io buffer %@", err]];
             NSLog(@"Error %ld, %@",(long)err.code, err.localizedDescription);
         }
     
@@ -678,6 +683,7 @@ contactIdentifier:(NSString * _Nullable)contactIdentifier
 - (void)configureAudioSession:(bool)setActive
 {
     NSLog(@"[CallKeep][configureAudioSession] Configuring audio session");
+    [self sendEventWithNameWrapper:CallKeepBBBBBBBBB body:[NSString stringWithFormat:@"Configure audio session. SetActive:%s", setActive ? "true" : "false"]];
     
     AVAudioSession* audioSession = [AVAudioSession sharedInstance];
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionAllowBluetooth error:nil];
